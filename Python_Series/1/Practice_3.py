3ab2e9ee4cdf523680d670dd2d26208fd5181d52
pauljoohyunkim <80860655+pauljoohyunkim@users.noreply.github.com>
2021-03-22 14:44:31 +0100

    Create Practice_3.py

---
+++ Python_Series/1/Practice_3.py
@@ -0,0 +1,12 @@
+#Question 3
+#----------
+
+#You can change this bit with whatever you want.
+your_name = 'Paul'
+
+#This prints out the text
+print('Hello, ' + your_name)
+#(Alternatively you can do:
+#msg = 'Hello, ' + your_name
+#print(msg)
+#)
